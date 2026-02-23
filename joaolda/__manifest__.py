ae7eee6cfd5c755280825620bf61b63a22011665
JGBotelho17 <jgbotelho17@gmail.com>
2026-02-23 12:13:31 +0000

    Modulo joaolda

---
+++ joaolda/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'JoaoLda: Gestão Solar',
+    'version': '1.0',
+    'author': 'Joao Botelho',
+    'category': 'Manufacturing',
+    'summary': 'Módulo de teste para CRM, Vendas e Produção Solar',
+    'depends': ['base', 'mrp', 'sale', 'stock'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
